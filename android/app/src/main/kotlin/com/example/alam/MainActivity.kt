ae07a48bfaad8bad512396fcf10843c2c562465e
ajmalm <ajmalmundeth1324@gmail.com>
2024-02-08 00:15:41 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/alam/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.alam
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/widegect/WeatherContainer.dart
@@ -0,0 +1,185 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:weather/weather.dart';
+
+class WeatherContainer extends StatefulWidget {
+  final String apiKey;
+
+  WeatherContainer({required this.apiKey});
+
+  @override
+  _WeatherContainerState createState() => _WeatherContainerState();
+}
+
+class _WeatherContainerState extends State<WeatherContainer> {
+  late WeatherFactory ws;
+  List<Weather> _data = [];
+  AppState _state = AppState.NOT_DOWNLOADED;
+  double? lat, lon;
+
+  @override
+  void initState() {
+    super.initState();
+    ws = WeatherFactory(widget.apiKey);
+    _getLocation();
+  }
+
+  Future<void> _getLocation() async {
+    try {
+      Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.high);
+      setState(() {
+        lat = position.latitude;
+        lon = position.longitude;
+      });
+    } catch (e) {
+      print("Error getting location: $e");
+    }
+  }
+
+  void queryForecast() async {
+    if (lat == null || lon == null) {
+      print('Latitude or longitude is null');
+      return;
+    }
+
+    setState(() {
+      _state = AppState.DOWNLOADING;
+    });
+
+    List<Weather> forecasts = await ws.fiveDayForecastByLocation(lat!, lon!);
+    setState(() {
+      _data = forecasts;
+      _state = AppState.FINISHED_DOWNLOADING;
+    });
+  }
+
+  void queryWeather() async {
+    if (lat == null || lon == null) {
+      print('Latitude or longitude is null');
+      return;
+    }
+
+    setState(() {
+      _state = AppState.DOWNLOADING;
+    });
+
+    Weather weather = await ws.currentWeatherByLocation(lat!, lon!);
+    setState(() {
+      _data = [weather];
+      _state = AppState.FINISHED_DOWNLOADING;
+    });
+  }
+
+  Widget contentFinishedDownload() {
+    return ListView.separated(
+      shrinkWrap: true,
+      itemCount: _data.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text(_data[index].toString()),
+        );
+      },
+      separatorBuilder: (context, index) {
+        return Divider();
+      },
+    );
+  }
+
+  Widget contentDownloading() {
+    return Container(
+      margin: EdgeInsets.all(25),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            'Fetching Weather...',
+            style: TextStyle(fontSize: 20),
+          ),
+          SizedBox(height: 50),
+          CircularProgressIndicator(strokeWidth: 10),
+        ],
+      ),
+    );
+  }
+
+  Widget contentNotDownloaded() {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            'Press the button to download the Weather forecast',
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _resultView() {
+    switch (_state) {
+      case AppState.FINISHED_DOWNLOADING:
+        return contentFinishedDownload();
+      case AppState.DOWNLOADING:
+        return contentDownloading();
+      default:
+        return contentNotDownloaded();
+    }
+  }
+
+  Widget _buttons() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Container(
+          margin: EdgeInsets.all(5),
+          child: TextButton(
+            child: Text(
+              'Fetch weather',
+              style: TextStyle(color: Colors.white),
+            ),
+            onPressed: lat != null && lon != null ? queryWeather : null,
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(Colors.blue),
+            ),
+          ),
+        ),
+        Container(
+          margin: EdgeInsets.all(5),
+          child: TextButton(
+            child: Text(
+              'Fetch forecast',
+              style: TextStyle(color: Colors.white),
+            ),
+            onPressed: lat != null && lon != null ? queryForecast : null,
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(Colors.blue),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        _buttons(),
+        SizedBox(height: 20),
+        Text(
+          'Output:',
+          style: TextStyle(fontSize: 20),
+        ),
+        Divider(
+          height: 20.0,
+          thickness: 2.0,
+        ),
+        Expanded(child: _resultView()),
+      ],
+    );
+  }
+}
+
+enum AppState { NOT_DOWNLOADED, DOWNLOADING, FINISHED_DOWNLOADING }
